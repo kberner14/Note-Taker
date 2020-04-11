e21b5617b6fab7b552c38fd300104b8bf4ccc7cc
Kyle Berner <kyleberner14@yahoo.com>
2020-04-10 22:19:17 -0700

    Wrong approach, had to completely start over

---
+++ Develop/public/server.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+// This middleware is responsible for constructing the
+// body property on the response object passed to our route handlers.
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// app.use(
+//   express.static(path.join(__dirname, "./public"), { extensions: ["html"] })
+// );
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "./Develop/public/index.html"));
+  });
+
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "./Develop/public/notes.html"));
+  });
+
+  app.get("/api/letters/:letters", (req, res) => {
+      return res.json(letters)
+  });
+
+app.listen(PORT, () => {
+    console.log("App listening on PORT " + PORT);
+  });
+  
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Note-Taker
+# Note Taker
+
+## Description
+
+* This assignment has caused me much grief, but I will not let it defeat me. I will be triumphant
